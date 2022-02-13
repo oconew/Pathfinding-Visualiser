3acb08784b147b20e8973ba5a3c646e6da6c75b0
Eathan O'Connor <37672460+oconew@users.noreply.github.com>
2022-02-13 21:08:56 +0000

    1.1 - A* Algorithm & Step by step visualisation
    - A* algorithm added and now works as expected.
    - Pathfinding is visualised step by step (node by node)
    - Added a parameter for the delay between steps.
    - Open set is now visualised as a darker green: this is determined to be the nodes in Q with dist < Infinity as these will be evaluated next.
    - Added an option for diagonal movement. However, this creates an error where a path between two walls can be made if the walls are diagonally adjacent.

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const start = {x: 0, y:0} // Co ords for starting node
-const target = {x: 10, y: 5}  // Co ords for target node
+const ANIM_DELAY = 20
 const scale = 50  // px side length of each cell
-const WIDTH = Math.floor(window.innerWidth/scale), HEIGHT = Math.floor(window.innerHeight/scale)  // Width and Height of map
+const parent = document.getElementById('grid')
+const WIDTH = Math.floor((window.innerWidth/100*100)/scale), HEIGHT = Math.floor((window.innerHeight/100*95)/scale)  // Width and Height of map
+const start = {x: 0, y:0} // Co ords for starting node
+const target = {x: Math.floor(WIDTH/2), y: Math.floor(HEIGHT/2)}  // Co ords for target node
+let map = []
 let Q = []  // Array containing all nodes in the set
-let end = {current: undefined, closed: undefined}
+let end
 
 class Node {
     /**
@@ -18,31 +21,41 @@ class Node {
         else if (this.x === target.x && this.y === target.y) {this.weight = 'T';}
         else {this.weight = 0}
         this.dist = Infinity
+        this.h = (this.x - target.x)**2 + (this.y-target.y)**2  // h is the estimated distance to target
         this.prev = undefined
-        document.getElementById('grid').innerHTML += `<div class="cell empty" id="${x}, ${y}" onclick="click_cell(this)" oncontectmenu="run()"></div>`
-
+        map[[x, y]] = this
+        document.getElementById('grid').innerHTML += `<div class="cell empty" id="${x}, ${y}" onclick="click_cell(this)" oncontectmenu="run()">${this.h}</div>`
     }
 
     get_neighbours() {
         let neighbours = [], neighbour
-        neighbour = Q[coord_to_Q(this.x, this.y)-1] // left neighbour
+        neighbour = map[[this.x-1, this.y]] // left neighbour
         if (neighbour !== undefined && this.x > neighbour.x)neighbours.push(neighbour)
 
-        neighbour=Q[coord_to_Q(this.x, this.y)-WIDTH] // top neighbour
+        neighbour= map[[this.x, this.y-1]] // top neighbour
         if (neighbour !== undefined && this.y > neighbour.y)neighbours.push(neighbour)
 
-        neighbour = Q[coord_to_Q(this.x, this.y)+1] // right neighbour
+        neighbour = map[[this.x+1, this.y]] // right neighbour
         if (neighbour !== undefined && this.x < neighbour.x)neighbours.push(neighbour)
 
-        neighbour=Q[coord_to_Q(this.x, this.y)+WIDTH] // bottom neighbour
+        neighbour = map[[this.x, this.y+1]] // bottom neighbour
         if (neighbour !== undefined && this.y < neighbour.y)neighbours.push(neighbour)
+
+
+        // uncomment to allow diagonal movement
+        // neighbour = map[[this.x-1, this.y-1]] // top left neighbour
+        // if (neighbour !== undefined)neighbours.push(neighbour)
+
+        // neighbour= map[[this.x+1, this.y-1]] // top right neighbour
+        // if (neighbour !== undefined)neighbours.push(neighbour)
+
+        // neighbour = map[[this.x-1, this.y+1]] // bottom left neighbour
+        // if (neighbour !== undefined)neighbours.push(neighbour)
+
+        // neighbour = map[[this.x+1, this.y+1]] // bottom right neighbour
+        // if (neighbour !== undefined)neighbours.push(neighbour)
         return neighbours
     }
-}
-
-
-function coord_to_Q(x, y) {
-    return x + (y*WIDTH)
 }
 
 function initialise_grid() {
@@ -56,29 +69,34 @@ function initialise_grid() {
 }
 
 function draw_grid() {
-    for (let node of Q) {
-        cell = document.getElementById(`${node.x}, ${node.y}`)
-        
-        switch (node.weight) {
-            case -1:
-                cell.className = "cell wall"
-                break;
-            case 'S':
-                cell.className = "cell start"
-                break;
-            case 'T':
-                cell.className = "cell target"
-                break;
-            case 'p':
-                cell.className = "cell path"
-                console.log('yup')
-                break;
-            default:
-                cell.className = "cell"
-                break;
-        }
-        if(node.dist < Infinity && Number.isInteger(node.weight)) {
-            cell.className = "cell closed"
+    for (let x = 0; x<WIDTH; x++) {
+        for (let y = 0; y<HEIGHT; y++) {
+            node = map[[x, y]]
+            cell = document.getElementById(`${node.x}, ${node.y}`)
+            switch (node.weight) {
+                case -1:
+                    cell.className = "cell wall"
+                    break;
+                case 'S':
+                    cell.className = "cell start"
+                    break;
+                case 'T':
+                    cell.className = "cell target"
+                    break;
+                case 'p':
+                    cell.className = "cell path"
+                    break;
+                default:
+                    cell.className = "cell"
+                    break;
+            }
+            // if(node.dist < Infinity && Number.isInteger(node.weight)) {
+            //     cell.className = "cell closed"
+            // }
+            if(isFinite(node.dist) && Number.isInteger(node.weight)) {
+                if(Q.includes(node))cell.className = "cell open"
+                else {cell.className = "cell closed"}
+            }
         }
     }
 }
@@ -91,38 +109,61 @@ function sleep(milliseconds) {
     } while (currentDate - date < milliseconds);
   }
   
+function changeClass(cell, new_class) {
+    cell.className = new_class
+}
 
-function djikstras() {
-    let open = [], closed = [], current, alt
-    open.push(Q[coord_to_Q(start.x, start.y)]) // add start node to open set
-    open[0].dist = 0  // start node has distance of 0
-    while (open.length > 0) {
-        open.sort((a, b) => (a.dist > b.dist) ? 1 : -1) // sort open array by distance
-        current = open.shift()  // evaluate node with lowest distance
-        if (current.weight ===  'T') {return {current, closed}}
+function dijkstras() {
+    let current, alt, cell
+    draw_grid()
+    map[[start.x, start.y]].dist = 0  // start node has distance of 0
+    if (Q.length > 0) {
+        Q.sort((a, b) => (a.dist > b.dist) ? 1 : -1) // sort open array by distance
+        current = Q.shift()  // evaluate node with lowest distance
+        if (current.weight ===  'T') {return {current}}
         for (let neighbour of current.get_neighbours()) {
             // Get new distances of neighbours
-            if (neighbour !== undefined && neighbour.weight !== -1 && !closed.includes(neighbour)) {
+            if (neighbour !== undefined && neighbour.weight !== -1 && Q.includes(neighbour)) {
                 if (neighbour.weight === 'p')neighbour.weight=0;
                 alt = current.dist + 1
                 if (alt < neighbour.dist || neighbour.dist === Infinity) {
                     neighbour.dist = alt
                     neighbour.prev = current
-                    //console.log(neighbour)
-                    open.push(neighbour)
                 }
             }
-        }
-        closed.push(current)
-        //sleep(100)
-        //console.log(Q[1].dist)
-        //draw_grid()
+        }        
+        setTimeout(dijkstras, ANIM_DELAY)
     }
+    draw_path(map[[target.x, target.y]])
 }
+
+function a_star() {
+    let current, alt
+    map[[start.x, start.y]].dist = 0
+    draw_grid()
+    if (Q.length > 0) {
+        Q.sort((a, b) => (a.dist + a.h > b.dist + b.h) ? 1 : -1) // sort open array by fScore
+        current = Q.shift()  // evaluate node with lowest distance
+        if (current.weight ===  'T') {return {current}}
+        for (let neighbour of current.get_neighbours()) {
+        if (neighbour !== undefined && neighbour.weight !== -1 && Q.includes(neighbour)) {
+            if (neighbour.weight === 'p')neighbour.weight=0;
+            alt = current.dist + neighbour.h
+            if (alt < neighbour.dist || neighbour.dist === Infinity) {
+                neighbour.dist = alt
+                neighbour.prev = current
+            }
+        }
+    }
+    setTimeout(a_star, ANIM_DELAY)
+    }
+    draw_path(map[[target.x, target.y]])
+}
+
 
 function draw_path(current) {
     // let current = Q[coord_to_Q(target.x, target.y)]
-    while (current.prev !== undefined && current.prev.weight !== 'S' && current.prev.weight !== 'T') {
+    while (current.prev !== undefined && current.prev.weight !== 'S') {
         current.prev.weight = 'p'
         current = current.prev
     }
@@ -130,25 +171,24 @@ function draw_path(current) {
 
 function click_cell(div) {
     let coords = div.id.split(', ')
-    cell = Q[coord_to_Q(parseInt(coords[0]), parseInt(coords[1]))]
+    cell = map[[coords[0], coords[1]]]
     if(cell.weight === -1) {cell.weight=0}
     else if (cell.weight === 'T' || cell.weight === 'S') {cell.weight = cell.weight}
     else {cell.weight = -1}
     if(cell.weight === 'T') {
         run()
     }
+    //draw_path(map[[target.x, target.y]])
     draw_grid()
 }
 
 initialise_grid()
 draw_grid()
 function run() {
-    console.log('ghvjfhnj')
-    let end = djikstras()
-    draw_path(end.current)
+
+    a_star()    
     draw_grid()
 }
 
 // n
-
 
